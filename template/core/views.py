ddc3e532b59a3c9abfaf8b3de2c32071425eb7fc
Bruno-H-Moraes <brunohmoraes93@gmail.com>
2021-03-21 19:26:33 -0300

    domingo 21/03

--- template/core/views.py
+++ template/core/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-
 # Create your views here.
 
 
 
 def index(request):
+    
     context = {
         'curso': 'Programação Web com Django'
+        
     }
     return render(request, 'index.html',context)
 
